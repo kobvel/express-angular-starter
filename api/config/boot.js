4059ab620775164311969d0c9e92c9035ef18c2e
sbaldani <sergio.baldani@gmail.com>
2016-08-18 14:28:25 -0300

    socket config on app

--- api/config/boot.js
+++ api/config/boot.js
@@ -1,5 +1,6 @@
 import https from 'https';
 import fs from 'fs';
+import sio from 'socket.io';
 
 module.exports = app => {
   console.log('Node Environment', process.env.NODE_ENV);
@@ -9,8 +10,26 @@ module.exports = app => {
       cert: fs.readFileSync('api/config/certs/mean.cert', 'utf8'),
     };
     app.db.sequelize.sync().done(() => {
-      https.createServer(credentials, app)
+      const server = https.createServer(credentials, app)
         .listen(app.get('port'), () => {
+          // socket config
+          const io = sio(server);
+          let channel = '';
+          const messages = []; // remove this - is just a test
+          io.sockets.on('connection', (socket) => {
+            socket.on('create', (room) => {
+              socket.join(room);
+              channel = room;
+            });
+          });
+          io.on('connection', (socket) => {
+            socket.on('send:conversation', (data) => {
+              messages.push(data); // remove this - is just a test
+              // send any object you want
+              io.sockets.in(channel).emit('send:conversation', messages);
+            });
+          });
+          // socket config end
           console.log(`MEAN API - Port ${app.get('port')}`);
         });
     });
